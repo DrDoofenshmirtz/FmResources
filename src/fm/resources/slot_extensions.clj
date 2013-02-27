29f32469eb52a5d8817e28de0a7f02d902084e98
FrankM <frank.mosebach@gmx.de>
2013-02-27 20:26:26 +0100

    Extensions for resource slots.
    Support for extending a resource's slots with a scope-based expiration policy.

---
+++ src/fm/resources/slot_extensions.clj
@@ -0,0 +1,24 @@
+(ns
+  ^{:doc "Extensions for resource slots."
+    :author "Frank Mosebach"}
+  fm.resources.slot-extensions
+  (:require
+    [fm.resources.store :as sto]))
+
+(defn- resource-expired-predicate [scope scopes]
+  (if-let [scopes (seq (drop-while (partial not= scope) scopes))]
+    (set scopes)
+    (throw (IllegalArgumentException.
+             (format "Scope %s is not in %s!" scope scopes)))))
+
+(defn- scope-expired-slot [scope scopes]
+  (let [resource-expired? (resource-expired-predicate scope scopes)]
+    (fn [resource expired-scope]
+      (if-not (resource-expired? expired-scope)
+        resource))))
+
+(defn with-scope [slots scope scopes]
+  (assoc slots ::scope-expired (scope-expired-slot scope scopes)))
+
+(defn scope-expired! [store scope]
+  (sto/send! ::scope-expired scope))
