db90c91ff9ea33fffa8e1fb13ec46cbedce13904
DrDoofenshmirtz <frank.mosebach@gmx.de>
2014-05-02 15:03:21 +0200

    Adjusted project naming scheme and dependency versions, removed local repo dependency.

--- project.clj
+++ project.clj
@@ -1,20 +1,10 @@
 ;; Leiningen project file for the FmResources clojure project.
-;;
-;; Additional dependencies:
-;;   fm-core.jar
 
-(def local-mvn-repo-path (-> (str (System/getProperty "user.home")
-                                  "/maven-repository")
-                             java.io.File.
-                             .toURI
-                             str))
-
-(defproject fm/resources "1.0.0"
+(defproject fm/fm-resources "1.0.0-SNAPSHOT"
   :description "FmResources: Resource (Lifecycle) Management."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [fm/fm-core "1.0.0"]]
-  :repositories {"mvn-local" ~local-mvn-repo-path}
+                 [fm/fm-core "1.0.0-SNAPSHOT"]]
   :aot [fm.resources.types]    
   :jar-name "fm-resources.jar"
   :omit-source false
